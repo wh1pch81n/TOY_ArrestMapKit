50e0a8402926332c1a21b706d813cac54f1b5f72
wh1pch81n <wh1pch81n@gmail.com>
2014-06-23 18:03:36 -0700

    hardcooding a coordinat at baltimore!

--- MapKit/RWTViewController.m
+++ MapKit/RWTViewController.m
@@ -8,6 +8,8 @@
 
 #import "RWTViewController.h"
 
+static const float METERS_PER_MILE = 1609.344;
+
 @interface RWTViewController ()
 
 @end
@@ -18,6 +20,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /**a structure that contains the geographical coordinates using the wgs 84 reference frame*/
+    CLLocationCoordinate2D zoomLocation; //well looky here its a c object
+    zoomLocation.latitude = 39.281516; //Picking a specific latitude aka baltimoore
+    zoomLocation.longitude = -76.580806; //picking a specific longitude aka baltimore
+    
+    /**Specify a location and specify how far out the fram should be.*/
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, //the center point
+                                                                       0.5 * METERS_PER_MILE, //distance from north to south.
+                                                                       0.5 * METERS_PER_MILE); //distance from east to west
+    /**tells the mapview to display the region.*/
+    [_mapView setRegion:viewRegion animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
