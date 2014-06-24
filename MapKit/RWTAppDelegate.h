0f45a7eb42414199428c21cf66604ebe73f4b13e
wh1pch81n <wh1pch81n@gmail.com>
2014-06-23 17:18:50 -0700

    Initial Commit

---
+++ MapKit/RWTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWTAppDelegate.h
+//  MapKit
+//
+//  Created by Derrick Ho on 6/23/14.
+//  Copyright (c) 2014 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapKit/RWTViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWTViewController.h
+//  MapKit
+//
+//  Created by Derrick Ho on 6/23/14.
+//  Copyright (c) 2014 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTViewController : UIViewController
+
+@end
